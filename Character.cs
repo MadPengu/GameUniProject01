2257fd2a4ed2b8f726663ee94a48ae55b3d99a48
MadPengu <36513397+MadPengu@users.noreply.github.com>
2018-03-01 06:00:31 +0000

    Add files via upload

---
+++ Character.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Character : MonoBehaviour {
+
+    [SerializeField]
+    private float speed;
+    protected Vector2 direction;
+    private Rigidbody2D myRigidbody;
+    // Use this for initialization
+
+    public bool IsMoving
+    {
+        get
+        {
+            return direction.x != 0 || direction.y != 0;
+        }
+    }
+    
+    void Start () {
+        myRigidbody = GetComponent<Rigidbody2D>();
+	}
+	
+
+	// Update is called once per frame
+	protected virtual void Update () {
+
+    }
+     
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        myRigidbody.velocity = direction.normalized * speed;
+    }
+}
+
---
+++ Stat.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Stat : MonoBehaviour {
+     private Image content;
+     private float currentFill;
+     public float MyMaxValue { get; set; }
+
+    private float currentValue;
+
+    public float MyCurrentValue
+    {
+        get
+        {
+            return currentValue;
+        }
+
+        set
+        {
+            currentValue = value;
+        }
+    }
+
+
+    // Use this for initialization
+    void Start () {
+        content = GetComponent<Image>();
+        content.fillAmount = 0.5;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
