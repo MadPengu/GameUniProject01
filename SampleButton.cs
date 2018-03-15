58dc0d7b6a98b195db4118e2157a3676f1e8ba75
sk9647f <36513930+sk9647f@users.noreply.github.com>
2018-03-15 12:12:19 +0000

    Add files via upload

---
+++ SampleButton.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class SampleButton : MonoBehaviour {
+    public Button buttonComponent;
+    public Text nameLabel;
+    public Image iconImage;
+    public Text priceText;
+
+    private Item item;
+
+    private ShopScrollList ScrollList;
+    // Use this for initialization
+    void Start ()
+    {
+        buttonComponent.onClick.AddListener(HandleClick);
+	}
+	
+	public void Setup(Item CurrentItem, ShopScrollList CurrentScrollList)
+    {
+        item = CurrentItem;
+        nameLabel.text = item.ItemName;
+        priceText.text = item.price.ToString();
+        iconImage.sprite = item.Icon;
+
+        ScrollList = CurrentScrollList;
+
+    }
+
+    public void HandleClick()
+    {
+        ScrollList.TransferItem(item); 
+    }
+}
---
+++ ShopScrollList.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+[System.Serializable]
+public class Item
+{
+    public string ItemName;
+    public Sprite Icon;
+    public float price = 1f;
+}
+
+public class ShopScrollList : MonoBehaviour {
+    public List<Item> ItemList;
+    public Transform ContentPanel;
+    public ShopScrollList OtherShop;
+    public Text MyGoldDisplay;
+    public SimpleObjectPool ButtonObjectPool;
+    public float Gold = 20;
+
+	// Use this for initialization
+	void Start () {
+        RefreshDisplay();
+    }
+
+    public void RefreshDisplay()
+    {
+        MyGoldDisplay.text = "Gold: " + Gold.ToString();
+        RemoveButtons();
+        AddButtons();
+        
+    }
+
+    private void AddButtons()
+    {
+        for (int i = 0; i < ItemList.Count; i++)
+        {
+            Item item = ItemList[i];
+            GameObject newButton = ButtonObjectPool.GetObject();
+            newButton.transform.SetParent(ContentPanel);
+
+            SampleButton sampleButton = newButton.GetComponent<SampleButton>();
+            sampleButton.Setup(item, this);
+        }
+    }
+
+    private void RemoveButtons()
+    {
+        while (ContentPanel.childCount > 0)
+        {
+            GameObject toRemove = transform.GetChild(0).gameObject;
+            ButtonObjectPool.ReturnObject(toRemove);
+        }
+    }
+    
+
+    public void TransferItem(Item Item)
+    {
+        if (OtherShop.Gold >= Item.price)
+        {
+            Gold += Item.price;
+            OtherShop.Gold -= Item.price;
+            AddItem(Item, OtherShop);
+            RemoveItem(Item, this);
+
+            RefreshDisplay();
+            OtherShop.RefreshDisplay();
+        }
+    }
+
+
+
+    private void AddItem(Item ItemToAdd, ShopScrollList ShopList)
+    {
+        ShopList.ItemList.Add(ItemToAdd);
+
+    }
+
+    
+
+    private void RemoveItem(Item itemToRemove, ShopScrollList shopList)
+    {
+        for (int i = shopList.ItemList.Count - 1; i >= 0; i--)
+        {
+            if (shopList.ItemList[i] == itemToRemove)
+            {
+                shopList.ItemList.RemoveAt(i);
+            }
+        }
+    }
+}
